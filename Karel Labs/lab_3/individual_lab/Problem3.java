e35dee450b807fe2f8d5b7a9c5900424107fd77f
annamstarr <31353295+annamstarr@users.noreply.github.com>
2017-09-08 10:58:02 -0700

    Lab 3 Final

--- Karel Labs/lab_3/individual_lab/Problem3.java
+++ Karel Labs/lab_3/individual_lab/Problem3.java
@@ -3,19 +3,42 @@ package lab_3.individual_lab;
 import kareltherobot.*;
 import java.awt.Color;
 
-public class Problem3 extends Robot
+public class Problem3 extends Problem2
 {
-   
     /**
      * Constructor for objects of class Problem1
      */
     public Problem3(int st, int av, Direction dir, int beep)
     {
         super(st, av, dir, beep);
+    } 
+    public void enterRoom() {
+        turnLeft();
+        for(int i = 0; i < 4; i++) {
+            if(frontIsClear()) {
+                move();
+                checkLeft();
+                checkRight();
+                faceNorth();
+            }
+        }  
     }
-
-    public void carpetRooms(){
-        
+    public void leaveRoom() {
+        faceSouth();
+        while (frontIsClear()) {
+            move();
+                }
+        }
+    public void checkLeft() {
+        faceWest();
+        if (frontIsClear()) {
+            leaveRoom();
+        }
+    }
+    public void checkRight() {
+        faceEast();
+        if (frontIsClear()) {
+            leaveRoom();
+        }
     }
 }
-
